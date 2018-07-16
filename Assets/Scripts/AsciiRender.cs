efabb61ed0d783d53014a50fa697d2690ca96504
Knar <knarxr@protonmail.com>
2018-07-15 20:16:59 -0500

    Added AsciiRender class

---
+++ Assets/Scripts/AsciiRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsciiRender : MonoBehaviour
+{
+    public CameraController renderCam;
+
+    public int[,] asciiArray;
+
+    private void Start()
+    {
+        asciiArray = new int[96, 54];
+    }
+
+    void OnPostRender()
+    {
+
+    }
+}
