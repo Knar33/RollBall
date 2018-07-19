b2da9ef88920842daf26bf2dcf55fdb3fbb7ce02
Knar33 <adbyerly@comcast.net>
2018-07-19 05:43:18 -0500

    Ascii much more optimized

--- Assets/Scripts/AsciiRender.cs
+++ Assets/Scripts/AsciiRender.cs
@@ -11,8 +11,14 @@ public class AsciiRender : MonoBehaviour
     public CameraController renderCam;
     public RenderTexture renderTexture;
     public TextMeshProUGUI renderText;
+    private char[] asciiCharArray;
     private char[] greyscaleAscii = new char[] { '$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', '\'', '.', ' ' };
-    
+
+    private void Start()
+    {
+        asciiCharArray = new char[58257];
+    }
+
     void Update()
     {
         Texture2D tex2d = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
@@ -22,19 +28,39 @@ public class AsciiRender : MonoBehaviour
 
         Color[] renderGrid = tex2d.GetPixels(0, 0, 320, 180);
 
-        StringBuilder sb = new StringBuilder("</mspace>");
+        asciiCharArray[0] = '<';
+        asciiCharArray[1] = 'm';
+        asciiCharArray[2] = 's';
+        asciiCharArray[3] = 'p';
+        asciiCharArray[4] = 'a';
+        asciiCharArray[5] = 'c';
+        asciiCharArray[6] = 'e';
+        asciiCharArray[7] = '=';
+        asciiCharArray[8] = '6';
+        asciiCharArray[9] = '>';
+        asciiCharArray[58248] = '<';
+        asciiCharArray[58249] = '/';
+        asciiCharArray[58250] = 'm';
+        asciiCharArray[58251] = 's';
+        asciiCharArray[58252] = 'p';
+        asciiCharArray[58253] = 'a';
+        asciiCharArray[58254] = 'c';
+        asciiCharArray[58255] = 'e';
+        asciiCharArray[58256] = '>';
         //TODO: try as char array
-        
+
         for (int y = 0; y < 180; y++)
         {
-            for (int x = 319; x > 0; x--)
+            for (int x = 0; x < 320; x++)
             {
-                sb.Append(getGreyscaleChar(Convert.ToDouble(renderGrid[x + (320 * y)].grayscale)).ToString());
+                int charSpace = 10 + (2 * y) + x + (320 * y);
+                asciiCharArray[charSpace] = getGreyscaleChar(Convert.ToDouble(renderGrid[x + (320 * y)].grayscale));
             }
-            sb.Append("\n");
+            asciiCharArray[10 + (2 * y) + 320 + (320 * y)] = '\\';
+            asciiCharArray[10 + (2 * y) + 320 + (320 * y) + 1] = 'n';
         }
-        sb.Insert(0, "<mspace=6>");
-        renderText.text = sb.ToString();
+        string output = new string(asciiCharArray);
+        renderText.text = output;
     }
 
     public char getGreyscaleChar(double hue)
